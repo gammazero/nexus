f0265111b0df38f57f498f8cf7713a4ad4ce3aca
Andrew Gillis <gillis.andrewj@gmail.com>
2017-08-01 00:53:13 -0400

    test all

---
+++ aat/doc.go
@@ -0,0 +1,7 @@
+/*
+Package AAT is a collection of Automated Acceptance Tests.  These test nexus
+functionality, end-to-end using websocket clients connected to a nexus WAMP
+router.
+
+*/
+package aat
