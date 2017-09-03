179e7fa39ca43b875daa825b923e2eaec09a4ffb
Andrew Gillis <gillis.andrewj@gmail.com>
2017-09-03 03:28:59 -0400

    Use stdout if log path not accessible.

--- nexusd/main.go
+++ nexusd/main.go
@@ -19,7 +19,7 @@ func usage() {
 func main() {
 	var cfgFile string
 	fs := flag.NewFlagSet("nexus", flag.ExitOnError)
-	fs.StringVar(&cfgFile, "c", "nexus.json", "Path to config file")
+	fs.StringVar(&cfgFile, "c", "etc/nexus.json", "Path to config file")
 	fs.Usage = usage
 	if err := fs.Parse(os.Args[1:]); err != nil {
 		os.Exit(1)
@@ -27,10 +27,27 @@ func main() {
 	// Read config file.
 	conf := LoadConfig(cfgFile)
 
+	var logger *log.Logger
+	if conf.LogPath == "" {
+		// If no log file specified, then log to stdout.
+		logger = log.New(os.Stdout, "", log.LstdFlags)
+	} else {
+		// Open the file to log to and set up logger.
+		f, err := os.OpenFile(conf.LogPath, os.O_RDWR|os.O_CREATE|os.O_APPEND,
+			0644)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		defer f.Close()
+		logger = log.New(f, "", log.LstdFlags)
+	}
+
 	// Create router and realms from config.
-	r, err := nexus.NewRouter(&conf.Router, nil)
+	r, err := nexus.NewRouter(&conf.Router, logger)
 	if err != nil {
-		log.Fatalln(err)
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 
 	// Create a new websocket server with the router.
@@ -42,18 +59,29 @@ func main() {
 	go func() {
 		// Shutdown server is SIGINT received.
 		<-shutdown
-		log.Println("shutting down server...")
-		s.Close()
+		fmt.Println("Shutting down router...")
+
 		// If process does not exit in a few seconds, exit with error.
-		time.Sleep(3 * time.Second)
-		os.Exit(1)
+		exitChan := make(chan struct{})
+		go func() {
+			select {
+			case <-time.After(5 * time.Second):
+				fmt.Fprintln(os.Stderr, "Router took too long to stop")
+				os.Exit(1)
+			case <-exitChan:
+			}
+		}()
+
+		s.Close()
+		close(exitChan)
+		os.Exit(0)
 	}()
 
-	// Run server on configured port.
+	// Run service on configured port.
 	server := &http.Server{
 		Handler: s,
 		Addr:    fmt.Sprintf(":%d", conf.Port),
 	}
-	log.Printf("server starting on port %d...", conf.Port)
+	fmt.Println("Router starting on port", conf.Port)
 	log.Fatalln(server.ListenAndServe())
 }
