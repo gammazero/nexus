025a424b29bfed3ffbdb26fd879e9abce99b2d0c
Andrew Gillis <gillis.andrewj@gmail.com>
2017-10-19 22:45:21 -0400

    pkg comments

--- router/router.go
+++ router/router.go
@@ -1,3 +1,9 @@
+/*
+Package router provides a WAMP router implementation that supports most of the
+WAMP advanced profile, offers multiple transports and TLS, and extends
+publication filtering functionality.
+
+*/
 package router
 
 import (
--- transport/doc.go
+++ transport/doc.go
@@ -1,6 +1,8 @@
 /*
-Package transport provides implementations of wamp.Peer that connect the Send
-and Recv methods to a particular transport.
+Package transport provides a websocket, rawsocket, and local transport
+implementation.  The local transport is for in-process connection of a client
+to a router.  Each transport implements the wamp.Peer interface, that connect
+Send and Recv methods to a particular transport.
 
 */
 package transport
