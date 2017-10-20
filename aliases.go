2c35acfd10c8fc28915a8adf132e3be25ff84892
Andrew Gillis <gillis.andrewj@gmail.com>
2017-10-19 21:41:24 -0400

    comments

--- aliases.go
+++ aliases.go
@@ -4,7 +4,6 @@ import (
 	"time"
 
 	"github.com/gammazero/nexus/router"
-	"github.com/gammazero/nexus/router/auth"
 	"github.com/gammazero/nexus/stdlog"
 )
 
@@ -12,22 +11,26 @@ type RouterConfig = router.RouterConfig
 type RealmConfig = router.RealmConfig
 type Router = router.Router
 
+// Alias for router.NewRouter
 func NewRouter(config *RouterConfig, logger stdlog.StdLog) (Router, error) {
 	return router.NewRouter(config, logger)
 }
 
 type WebsocketServer = router.WebsocketServer
 
+// Alias for router.NewWebsocketServer
 func NewWebsocketServer(r Router) *WebsocketServer {
 	return router.NewWebsocketServer(r)
 }
 
 type RawSocketServer = router.RawSocketServer
 
+// Alias for router.NewRawSocketServer
 func NewRawSocketServer(r Router, recvLimit int, keepalive time.Duration) *RawSocketServer {
 	return router.NewRawSocketServer(r, recvLimit, keepalive)
 }
 
 type Authorizer = router.Authorizer
 
-type Authenticator = auth.Authenticator
+// Alias for router.NewAuthorizer
+func NewAuthorizer() Authorizer { return router.NewAuthorizer() }
