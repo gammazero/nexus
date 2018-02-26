60824ba2cf1794c80d83aff2f7579d07454d6304
Andrew J. Gillis <gammazero@users.noreply.github.com>
2018-02-26 00:12:24 -0800

    Remove default dummy Authorizer implementation, and do not create one if no authorzer is given.  Instead, avoid using if nil. (#82)

--- router/authorizer.go
+++ router/authorizer.go
@@ -3,29 +3,19 @@ package router
 import "github.com/gammazero/nexus/wamp"
 
 // Authorizer is the interface implemented by a type that provides the ability
-// to authorize request messages.
+// to authorize sending messages.
 type Authorizer interface {
-	// Authorize returns true if the request is authorized or false if not.  An
-	// error is returned if there is a failure to determine authorization.
-	// This error is included in the ERROR response to the client.
+	// Authorize returns true if the sending session is authorized to send the
+	// message.  Otherwise, it returns or false if not authorized.  An error is
+	// returned if there is a failure to determine authorization.  This error
+	// is included in the ERROR response to the client.
 	//
-	// Since the Authorizer accesses both the session and the message through a
-	// pointer, the authorizer can alter the content of both the session and
-	// the message.  This allows the authorizer to also work as an interceptor
-	// of messages to change their content or change the sending session based
-	// on the intercepted message.
+	// Since the Authorizer accesses both the sending session and the message
+	// through a pointer, the authorizer can alter the content of both the
+	// sending session and the message.  This allows the authorizer to also
+	// work as an interceptor of messages that can change their content and/or
+	// change the sending session based on the intercepted message.  This
+	// functionality may be used to set values in the session upon encountering
+	// certain messages sent by that session.
 	Authorize(*wamp.Session, wamp.Message) (bool, error)
-}
-
-// authorizer is the default implementation that always returns authorized.
-type authorizer struct{}
-
-// NewAuthorizer returns the default authorizer.
-func NewAuthorizer() Authorizer {
-	return &authorizer{}
-}
-
-// Authorize default implementation authorizes any session for all roles.
-func (a *authorizer) Authorize(sess *wamp.Session, msg wamp.Message) (bool, error) {
-	return true, nil
 }
