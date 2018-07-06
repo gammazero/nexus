3fb387414ea8886726adc1434622c7a3802c804d
Andrew Gillis <gillis.andrewj@gmail.com>
2018-07-06 02:49:40 -0400

    Cleanup and take care of misc details
    - Add json tags for new realm config items
    - Add config items to nexusd default config file
    - Add example that shows some use of session meta API
    - Clean up some code and documentation

---
+++ examples/session_meta_api/session_meta_client.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gammazero/nexus/client"
+	"github.com/gammazero/nexus/examples/newclient"
+	"github.com/gammazero/nexus/wamp"
+)
+
+const (
+	metaOnJoin  = string(wamp.MetaEventSessionOnJoin)
+	metaOnLeave = string(wamp.MetaEventSessionOnLeave)
+
+	metaCount = string(wamp.MetaProcSessionCount)
+	metaList  = string(wamp.MetaProcSessionList)
+	metaGet   = string(wamp.MetaProcSessionGet)
+)
+
+func subscribeMetaOnJoin(subscriber *client.Client, logger *log.Logger) {
+	// Define function to handle on_join events received.
+	onJoin := func(args wamp.List, kwargs wamp.Dict, details wamp.Dict) {
+		details, ok := wamp.AsDict(args[0])
+		if !ok {
+			logger.Println("Client joined realm - no data provided")
+			return
+		}
+		onJoinID, _ := wamp.AsID(details["session"])
+		authid, _ := wamp.AsString(details["authid"])
+		logger.Printf("Client %v joined realm (authid=%s)\n", onJoinID, authid)
+	}
+
+	// Subscribe to on_join topic.
+	err := subscriber.Subscribe(metaOnJoin, onJoin, nil)
+	if err != nil {
+		logger.Fatal("subscribe error:", err)
+	}
+	logger.Println("Subscribed to", metaOnJoin)
+}
+
+func subscribeMetaOnLeave(subscriber *client.Client, logger *log.Logger) {
+	// Define function to handle on_leave events received.
+	onLeave := func(args wamp.List, kwargs wamp.Dict, details wamp.Dict) {
+		id, ok := wamp.AsID(args[0])
+		if !ok {
+			logger.Println("A client left the realm")
+			return
+		}
+		logger.Printf("Client", id, "left realm")
+	}
+
+	// Subscribe to on_join topic.
+	err := subscriber.Subscribe(metaOnLeave, onLeave, nil)
+	if err != nil {
+		logger.Fatal("subscribe error:", err)
+	}
+	logger.Println("Subscribed to", metaOnLeave)
+}
+
+func getSessionCount(caller *client.Client, logger *log.Logger) {
+	// Call meta procedure to get session count.
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	result, err := caller.Call(ctx, metaCount, nil, nil, nil, "")
+	if err != nil {
+		logger.Println("Call error:", err)
+		return
+	}
+	count, _ := wamp.AsInt64(result.Arguments[0])
+	logger.Println("Current session count:", count)
+}
+
+func main() {
+	logger := log.New(os.Stdout, "METACLIENT> ", 0)
+	// Connect subscriber client with requested socket type and serialization.
+	cli, err := newclient.NewClient(logger)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer cli.Close()
+
+	// Check for feature support in router.
+	const featureSessionMetaAPI = "session_meta_api"
+	if !cli.HasFeature("broker", featureSessionMetaAPI) {
+		logger.Fatal("Broker does not have", featureSessionMetaAPI, "feature")
+	}
+	if !cli.HasFeature("dealer", featureSessionMetaAPI) {
+		logger.Fatal("Dealer does not have", featureSessionMetaAPI, "feature")
+	}
+
+	subscribeMetaOnJoin(cli, logger)
+	subscribeMetaOnLeave(cli, logger)
+
+	getSessionCount(cli, logger)
+
+	// Wait for CTRL-c or client close while handling events.
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	select {
+	case <-sigChan:
+	case <-cli.Done():
+		logger.Print("Router gone, exiting")
+		return // router gone, just exit
+	}
+
+	// Unsubscribe from topic.
+	if err = cli.Unsubscribe(metaOnJoin); err != nil {
+		logger.Println("Failed to unsubscribe:", err)
+	}
+	if err = cli.Unsubscribe(metaOnLeave); err != nil {
+		logger.Println("Failed to unsubscribe:", err)
+	}
+}
