d74a28ad667a553cbc204d21c460796b6284e5b5
ronanj <ronan.jezequel@gmail.com>
2018-07-31 09:01:07 +0800

    improved nodejs/go interop testcase

--- examples/interop/wampy-nodejs/caller.ts
+++ examples/interop/wampy-nodejs/caller.ts
@@ -14,14 +14,22 @@ function parallelCall(broker:Broker, duration: number) {
     let st = Date.now(), lt = st
     while (Date.now()-st<duration) {
         if (Date.now()-lt>1000) {
-            console.log(new Date(),promises.length,"calls")
+            let cps = Math.round(promises.length*1000 / (Date.now()-st))
+            console.log( "Calls per second: ", cps ,"calls/sec [",promises.length," calls]");
             lt = Date.now()
 
         }
         promises.push(broker.rpc("server.time").catch(err=>0))
     }
-    console.log( "Call per second: ", promises.length / duration,"obj/sec");
-    return Promise.all(promises)
+    let cps = Math.round(promises.length*1000 / duration)
+    console.log( "Calls per second: ", cps ,"calls/sec [",promises.length," calls]");
+    return Promise.all(promises).then(_=>{
+
+        duration = Date.now()-st
+        let cps = Math.round(promises.length*1000 / duration)
+        console.log( "All calls finished in", duration ,"ms - That's ",cps,"calls/sec");
+
+    })
 
 }
 
