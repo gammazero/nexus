bcb829827733f72febac3ca1cb7f3649d8a5ed48
ronan <ronan.jezequel@gmail.com>
2018-08-12 04:19:43 +0800

    Add nodejs/go interop example (#139)

---
+++ examples/interop/wampy-nodejs/caller.ts
@@ -0,0 +1,20 @@
+
+import {WampClient} from "./client"
+
+function main() {
+
+    let sumServer = new WampClient({realm:"nexus.examples",port:8000})
+
+    let arrayToSum = [1,2,3,4,5]
+
+    sumServer.rpc("sum",arrayToSum).then(result=>{
+        console.log("The sum of ",arrayToSum," is ",result)
+
+    }).catch(err=>{
+        console.log("Ooops, something went wrong:", err)
+
+    })
+
+}
+
+main()
---
+++ examples/interop/wampy-nodejs/client.ts
@@ -0,0 +1,75 @@
+import { Wampy } from "wampy";
+
+export class WampClient {
+    connectP: Promise<any>;
+    ws: any;
+
+    constructor(private options: { debug?: boolean, port?:number, realm?:string } = { debug: false, port: 8008 }) {
+    }
+
+    connect(): Promise<any> {
+        if (!this.connectP)
+            this.connectP = new Promise<any>((resolve, reject) => {
+                this.log("connecting...");
+                let w3cws = require("websocket").w3cwebsocket;
+                this.ws = new Wampy("ws://127.0.0.1:"+this.options.port+"/", {
+                    ws: w3cws,
+                    realm: this.options.realm||"nexus.example",
+                    onConnect: () => {
+                        this.log("connection - connect success");
+                        resolve(this.ws);
+                    },
+                    onError: err => {
+                        this.log("connection - error!");
+                        reject(err);
+                    },
+                    onClose: () => {
+                        this.log("connection - closed");
+                    },
+                    debug: false,
+                    autoReconnect: true
+                });
+            });
+        return this.connectP;
+    }
+
+    close() {
+        this.log("closing");
+        this.ws.disconnect();
+        this.connectP = null;
+        this.ws = null;
+    }
+
+    log(...args) {
+        console.log("[broker]", ...args);
+    }
+
+    subscribe(topic, callback) {
+        this.log("Subscribing to ", topic);
+        this.connect().then(ws => {
+            ws.subscribe(topic, v => {
+                this.log("Message received");
+                callback(v);
+            });
+        });
+    }
+
+    rpc(method,args = {}): Promise<any> {
+        return this.connect().then( ws => 
+            new Promise<any>((resolve, reject) => {
+                let r = ws.call( method, args,
+                    {
+                        onSuccess: (dataArr, dataObj) => {
+                            let result = dataArr.argsList;
+                            resolve(...result);
+                        },
+                        onError: (err, detailsObj) => {
+                            this.log("call failed with error ", err);
+                            reject(err);
+                        }
+                    }
+                );
+            })
+        );
+    }
+}
