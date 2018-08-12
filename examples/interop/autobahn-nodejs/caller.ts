ffae9d74358c1d36c179aba4c8a50c415e8c1c02
ronan <ronan.jezequel@gmail.com>
2018-08-12 12:10:13 +0800

    autobahn-js interop example  (#147)
    * NodeJS interop example update
    * Autobahn nodejs interop example

---
+++ examples/interop/autobahn-nodejs/caller.ts
@@ -0,0 +1,29 @@
+
+import {AuthobanClient} from "./client"
+
+function main() {
+
+    let arithmeticServer = new AuthobanClient({realm:"nexus.examples",port:8000})
+
+    let arrayToSum = [1,2,3,4,5]
+
+    arithmeticServer.rpc("sum",arrayToSum).then(result=>{
+        console.log("The sum of ",arrayToSum," is ",result)
+
+    }).catch(err=>{
+        console.log("Ooops, something went wrong:", err)
+
+    })
+
+    arithmeticServer.rpc("div",[1,2]).then(result=>{
+        console.log("The div of ",arrayToSum," is ",result)
+
+    }).catch(err=>{
+        console.log("Ooops, something went wrong:", err)
+
+    })
+
+
+}
+
+main()
---
+++ examples/interop/autobahn-nodejs/client.ts
@@ -0,0 +1,67 @@
+import { Connection } from "autobahn";
+
+export class AuthobanClient {
+
+    connectP: Promise<any>;
+    connection: Connection;
+
+    constructor(private options: { debug?: boolean, port?:number, realm?:string } = { debug: false, port: 8008 }) {
+    }
+
+    connect(): Promise<any> {
+        if (!this.connectP) {
+
+            this.connectP = new Promise((resolve,reject)=>{
+
+                let realm = this.options.realm||"nexus.example"
+                let url = "ws://127.0.0.1:"+this.options.port+"/"
+
+                let connection = new Connection({url, realm});
+                this.connection = connection;
+
+                connection.onopen = session=>{
+                    this.log("connection - connect success");
+                    resolve(session);
+                }
+
+                connection.onclose = (reason, details)=>{
+                    this.log("connection - closed: ",reason);
+                }
+
+                connection.open();
+            });
+        }
+
+
+        return this.connectP;
+    }
+
+    close() {
+        this.log("closing");
+        this.connection.close();
+        this.connection = null;
+        this.connectP = null;
+    }
+
+    log(...args) {
+        console.log("[broker]", ...args);
+    }
+
+    subscribe(topic, callback) {
+        this.log("Subscribing to ", topic);
+        this.connect().then(ws => {
+            ws.subscribe(topic, v => {
+                this.log("Message received");
+                callback(v);
+            });
+        });
+    }
+
+    rpc(method,args = {}): Promise<any> {
+        return this.connect().then( ws => {
+            return ws.call( method, args  ).catch(e=>{
+                throw(e.error)
+            });
+        });
+    }
+}
