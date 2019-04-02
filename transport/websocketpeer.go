10b19196c0b586baca06dfc4902e8b4d06ba4f11
Bryan Kneis <bryan.kneis@veeasystems.com>
2019-04-02 09:01:45 +0100

    Defer the contexts cancel

--- transport/websocketpeer.go
+++ transport/websocketpeer.go
@@ -116,7 +116,9 @@ func ConnectWebsocketPeer(routerURL string, serialization serialize.Serializatio
 		dialer.EnableCompression = true
 	}
 
-	ctx, _ := context.WithTimeout(context.Background(), 5 * time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+	
 	conn, _, err := dialer.DialContext(ctx, routerURL, nil)
 	if err != nil {
 		return nil, err
