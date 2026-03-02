a753cfea3ea3b25a41bc0a0bfaf0a35b927ab6b9
gammazero <11790789+gammazero@users.noreply.github.com>
2026-03-01 23:17:26 -1000

    package in test directory is now test_test

--- test/doc.go
+++
@@ -1,3 +0,0 @@
-// Package test is a collection of end-to-end tests that test nexus using
-// websocket clients connected to a nexus WAMP router.
-package test
---
+++ test/doc_test.go
@@ -0,0 +1,3 @@
+// Package test_test is a collection of end-to-end tests that test nexus using
+// websocket clients connected to a nexus WAMP router.
+package test_test
